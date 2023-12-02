016f1581007db816c41f7177e2fb097406c04c1c
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2023-12-02 06:36:55 +0000

    solution

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -29,6 +29,14 @@ class BirthdayCake
     self.lit = false
   end
 
+  def light_candles
+    return self.lit = true
+  end
+
+  def blow_out_candles
+    return self.lit = false
+  end
+
   def candles_status
     if self.lit == true
       return "lit"
@@ -38,7 +46,7 @@ class BirthdayCake
   end
 
   def greet
-    "Happy #{self.age.ordinalize} Birdday!"
+    "Happy #{self.age.to_i} Birthday!"
   end
 
   def sing
@@ -49,17 +57,18 @@ class BirthdayCake
     "Birthday cake with #{self.age} #{candles_status} candles"
   end
 
+
   def BirthdayCake.celebrate(age)
-    birthday_cake = Birthdaycake.new(age)
+    birthday_cake = BirthdayCake.new(age)
     puts birthday_cake.greet
     puts birthday_cake.to_s
     puts "Lighting candles..."
     birthday_cake.light_candles
     puts birthday_cake.to_s
     puts "Singing happy birthday..."
-    puts birthday_cake.sign
+    puts birthday_cake.sing
     puts "Blowing out candles..."
-    birth_cake.blow_out_candles
+    birthday_cake.blow_out_candles
     puts birthday_cake.to_s
 
     birthday_cake
